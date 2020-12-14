3137ab3a7d8414c7c1ccef4c80e45662fed01026
Steven Sojka <Steven.Sojka@tdameritrade.com>
2020-12-14 11:14:42 -0600

    replace vim-plug with packer.nvim

---
+++ .vim/init.lua
@@ -0,0 +1,5 @@
+vim.cmd [[let mapleader = "\<Space>"]]
+vim.cmd [[let g:mapleader = "\<Space>"]]
+
+require "plugins"
+require "aniseed.env".init({ module = "dotfiles.init" })
---
+++ .vim/lua/plugins.lua
@@ -0,0 +1,72 @@
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	vim.cmd("!git clone https://github.com/wbthomason/packer.nvim ".. install_path)
+end
+
+vim.cmd "packadd packer.nvim"
+
+require "packer".startup(function()
+  use {"wbthomason/packer.nvim", opt = true}
+  use "liuchengxu/vim-which-key"
+  use {
+    "junegunn/fzf",
+    run = function(conf)
+      vim.cmd(string.format("!%s/install --all", conf.install_path))
+      vim.cmd(string.format("!ln -s %s ~/.fzf", conf.install_path))
+    end}
+  use "junegunn/fzf.vim"
+  use "mhartington/oceanic-next"
+  use "tpope/vim-commentary"
+  use "itchyny/lightline.vim"
+  use "justinmk/vim-sneak"
+  use "tpope/vim-fugitive"
+  use {"mbbill/undotree", cmd = "UndotreeToggle"}
+  use "tpope/vim-surround"
+  use "justinmk/vim-dirvish"
+  use "kristijanhusak/vim-dirvish-git"
+  use "arthurxavierx/vim-caser"
+  use "mhinz/vim-startify"
+  use "norcalli/nvim-colorizer.lua"
+  use "tpope/vim-dispatch"
+  use {
+    "so-fancy/diff-so-fancy",
+    run = function(conf)
+      vim.cmd(string.format("!ln -s %s ~/.diff-so-fancy", conf.install_path))
+    end}
+  use "nvim-lua/completion-nvim"
+  use "neovim/nvim-lspconfig"
+  use "airblade/vim-gitgutter"
+  use "editorconfig/editorconfig-vim"
+  use {"prettier/vim-prettier", run = "npm install"}
+  use "dense-analysis/ale"
+  use {
+    "steelsojka/completion-buffers",
+    after = "completion"}
+  use "raimondi/delimitmate"
+  use "sheerun/vim-polyglot"
+  use {
+    "nvim-treesitter/playground",
+    after = "nvim-treesitter"}
+  use {
+    "nvim-treesitter/nvim-treesitter-refactor",
+    after = "nvim-treesitter"}
+  use "~/src/nvim-treesitter"
+  use {
+    "~/src/nvim-treesitter-angular",
+    after = "nvim-treesitter"}
+  use "norcalli/snippets.nvim"
+  use {
+    "nvim-telescope/telescope.nvim",
+    commit = "62b8655f1532d92245a50ac387201f2d1ac616e1",
+    requires = {
+      "nvim-lua/popup.nvim",
+      "nvim-lua/plenary.nvim"}}
+  use {
+    "vigoux/architext.nvim",
+    after = "nvim-treesitter"}
+  use "jpalardy/vim-slime"
+  use "bakpakin/fennel.vim"
+  use {"Olical/aniseed", branch = "develop"}
+  use {"Olical/conjure", tag = "v4.9.0"}
+end)
